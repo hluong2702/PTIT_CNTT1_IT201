81ebc28bdeea9cdef301644d854be7058b9e83d9
hluong2702 <hoangluong2702@gmail.com>
2025-07-06 21:33:51 +0700

    update

--- PTIT_CNTT1_IT201_Session11/PTIT_CNTT1_IT201_Session10_Bai01.c
+++ PTIT_CNTT1_IT201_Session11/PTIT_CNTT1_IT201_Session10_Bai01.c
@@ -19,11 +19,18 @@ int main() {
     Node* node1 = createNode(1);
     Node* node2 = createNode(2);
     Node* node3 = createNode(3);
+    Node* node4 = createNode(4);
+    Node* node5 = createNode(5);
 
     node1->next = node2;
     node2->prev = node1;
     node2->next = node3;
     node3->prev = node2;
+    node3->next = node4;
+    node4->prev = node3;
+    node4->next = node5;
+    node5->prev = node4;
+    node5->next = NULL;
 
     return 0;
 }
